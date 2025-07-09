c9f8a556c97eb73574fce60d25303463b63ab1b3
Kolkative <kolkative@gmail.com>
2025-07-10 00:27:28 +0700

    Perbarui ikon tema dengan mengganti elemen sun dan moon menjadi SVG untuk meningkatkan kualitas tampilan dan konsistensi visual.

--- src/page-script.js
+++ src/page-script.js
@@ -52,8 +52,13 @@ window.onload = function () {
   const THEME_KEY = "theme";
   const NOTION_DARK_CLASS = "notion-dark-theme";
 
-  const sunIcon = '<i class="hgi hgi-stroke hgi-sun-03"></i>';
-  const moonIcon = '<i class="hgi hgi-stroke hgi-moon-02"></i>';
+  const sunIcon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
+    <path d="M17 12C17 14.7614 14.7614 17 12 17C9.23858 17 7 14.7614 7 12C7 9.23858 9.23858 7 12 7C14.7614 7 17 9.23858 17 12Z" stroke="currentColor" stroke-width="1.5"></path>
+    <path d="M12 2C11.6227 2.33333 11.0945 3.2 12 4M12 20C12.3773 20.3333 12.9055 21.2 12 22M19.5 4.50271C18.9685 4.46982 17.9253 4.72293 18.0042 5.99847M5.49576 17.5C5.52865 18.0315 5.27555 19.0747 4 18.9958M5.00271 4.5C4.96979 5.03202 5.22315 6.0763 6.5 5.99729M18 17.5026C18.5315 17.4715 19.5747 17.7108 19.4958 18.9168M22 12C21.6667 11.6227 20.8 11.0945 20 12M4 11.5C3.66667 11.8773 2.8 12.4055 2 11.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path>
+</svg>';
+  const moonIcon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
+    <path d="M21.5 14.0784C20.3003 14.7189 18.9301 15.0821 17.4751 15.0821C12.7491 15.0821 8.91792 11.2509 8.91792 6.52485C8.91792 5.06986 9.28105 3.69968 9.92163 2.5C5.66765 3.49698 2.5 7.31513 2.5 11.8731C2.5 17.1899 6.8101 21.5 12.1269 21.5C16.6849 21.5 20.503 18.3324 21.5 14.0784Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+</svg>';
 
   let currentTheme;
 
