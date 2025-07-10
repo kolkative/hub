3bdcb018beff24277003b362d89bcd6267ccb01f
Kolkative <kolkative@gmail.com>
2025-07-10 02:30:40 +0000

    new

--- src/site-config.ts
+++ src/site-config.ts
@@ -88,7 +88,7 @@ export const SITE_CONFIG: NoteHostSiteConfig = {
       --surface-hover: #454952;
     }
 
-    .notion-frame {
+    .notion-body {
       background-color: var(--bg-main) !important;
       color: var(--text-main) !important;
     }
