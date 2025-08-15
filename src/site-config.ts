853583f2e91226ecd46c45b427414355616a7002
Kolkative <kolkative@gmail.com>
2025-08-15 04:09:15 +0000

    update

--- src/site-config.ts
+++ src/site-config.ts
@@ -673,7 +673,7 @@ export const SITE_CONFIG: NoteHostSiteConfig = {
     }
     #x-sidebar .sidebar-section {
       gap: 6px;
-      margin: 16px 20px 8px 30px;
+      margin: 16px 20px 9px 30px;
       font-size: 0.95rem;
       color: var(--text-2);
       letter-spacing: 0.3px;
