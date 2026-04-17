ac242d6b311dd3af818e96cece30ab57c28c1cdf
Jo <kolkative@gmail.com>
2026-04-18 01:59:53 +0700

    nccf added

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,6 @@ initializeReverseProxy(SITE_CONFIG);
 
 export default {
   async fetch(request: Request): Promise<Response> {
-    const url = new URL(request.url);
-
-    // handle subdomain nccf
-    if (
-      url.hostname === "nccf.kolkative.my.id" &&
-      !url.pathname.startsWith("/nccf")
-    ) {
-      url.pathname = `/nccf${url.pathname}`;
-      request = new Request(url.toString(), request);
-    }
-
     return await reverseProxy(request);
   },
 };
--- src/site-config.ts
+++ src/site-config.ts
@@ -63,7 +63,10 @@ export const SITE_CONFIG: NoteHostSiteConfig = {
     www: {
       redirect: "https://hub.kolktive.my.id",
     },
-  },
+    nccf: {
+      redirect: "https://hub.kolktive.my.id/nccf",
+    },
+  }
 
   // The 404 (not found) page is optional
   // If you don't have one, the default 404 page will be used
