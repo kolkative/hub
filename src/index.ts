fcfd1619b9eed30e551f6f4bceacd80cd61cc9e1
Jo <kolkative@gmail.com>
2026-04-18 01:39:26 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-import { initializeReverseProxy, reverseProxy } from 'notehost'
-import { SITE_CONFIG } from './site-config'
+import { initializeReverseProxy, reverseProxy } from "notehost";
+import { SITE_CONFIG } from "./site-config";
 
-initializeReverseProxy(SITE_CONFIG)
+initializeReverseProxy(SITE_CONFIG);
 
 export default {
   async fetch(request: Request): Promise<Response> {
-    return await reverseProxy(request)
+    const url = new URL(request.url);
+
+    // handle subdomain nccf
+    if (
+      url.hostname === "nccf.kolkative.my.id" &&
+      !url.pathname.startsWith("/nccf")
+    ) {
+      url.pathname = `/nccf${url.pathname}`;
+      request = new Request(url.toString(), request);
+    }
+
+    return await reverseProxy(request);
   },
-}
+};
